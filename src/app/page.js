e92f9916f2b67cbea048bdfd827ef3a08cb64f2e
Tether-creator <gruthzillions@gmail.com>
2025-03-15 18:00:53 +0100

    Delete src/app directory

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="container">
-      <img src="/dragon-logo.png" alt="Dragon Flash Logo" className="logo" />
-      <h1 className="header">Dragon Flash Wallet</h1>
-      <button className="connect-btn">Connect Wallet</button>
-    </div>
-  );
-}
